425708d787ab0a4cd64f674163dc0b28ea4aa5cd
ABCDE09876 <135597193+ABCDE09876@users.noreply.github.com>
2023-12-16 23:13:33 +0900

    Add files via upload

---
+++ 장학금계산프로그렘.py
@@ -0,0 +1,11 @@
+print("===장학금 대상자 선별 프로그렘===")
+score=int(input("점수를 입력하세요:"))
+if score>=90:
+    print("장학금 대상자입니다")
+    print("ㅊㅋㅊㅋ")
+else:
+    print("장학금 대상자 아닙니다")
+    print("바보")
+
+print("수고하셨습니다")
+input()
